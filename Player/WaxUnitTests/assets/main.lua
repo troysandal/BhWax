7eaeb97dbadd0c377356232147dcb41c7509dc06
Troy Sandal <troysandal@gmail.com>
2025-09-11 20:08:12 -0700

    Gideros Wax Unit Test project

---
+++ Player/WaxUnitTests/assets/main.lua
@@ -0,0 +1,17 @@
+--[[
+  Wax Unit Tests for Gideros Luau
+  
+  Gideros has it's own, precompiled, Luau engine which comes with some 
+  peculialrities that make testing it harder. Normally Wax compiles directly
+  with the Lua 5.1 sources and you run the unit tests by setting the
+  WAX_TEST env variable to YES in the wax/tools/Tests project.  But not for Luau.  
+ 
+  To run the unit tests we've added them into this Gideros project which you 
+  need to run using the WaxPlayer that has the Classes folder added from 
+  wax/tools/Tests
+]]--
+
+--require "wax"
+require "wax.init"
+
+require "tests"
