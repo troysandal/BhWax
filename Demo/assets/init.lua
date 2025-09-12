07580ec895aa4e6ad54089e0945c7d3507d86cf0
Troy Sandal <troysandal@gmail.com>
2025-09-12 14:48:39 -0700

    redundant code
    it's already in wax/init.lua

--- Demo/assets/init.lua
+++ Demo/assets/init.lua
@@ -4,22 +4,6 @@
 require "wax"
 require "wax.init"
 
--- This metatable magic allows for undefined Lua globals to be looked up in
--- the Wax class dictionary.
-
-setmetatable(_G, {
-	__index = function(self, key)
-    local class = wax.class[key]
-    if class then self[key] = class end -- cache it for future use
-    
-    if not class and key:match("^[A-Z][A-Z][A-Z]") then -- looks like they were trying to use an objective-c obj
-      print("WARNING: No object named '" .. key .. "' found.")
-    end
-    
-    return class
-  end
-})
-
 -- Temporary fix for the issue of device orientation when operating under the Gideros player.
 -- http://www.giderosmobile.com/forum/discussion/1737/played-with-xcode-4.5-and-ios6#Item_8
 -- Fix is from:
